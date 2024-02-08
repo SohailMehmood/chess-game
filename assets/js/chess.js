431f73094feb4ebed0f4153d6cb2e6b6d9f73816
SohailMehmood <sohail_mehmood@hotmail.com>
2024-02-08 15:50:40 +0000

    Add colour to the chessboard

--- assets/js/chess.js
+++ assets/js/chess.js
@@ -14,3 +14,25 @@ function insertImages() {
 }
 
 insertImages();
+
+//Coloring the board
+function colorBoard() {
+    const color = document.querySelectorAll('.square')
+
+    color.forEach((color => {
+        getId = color.id;
+        arr = Array.from(getId);
+        arr.shift();
+        aside = eval(arr.pop());
+        aup = eval(arr.shift());
+        a = aside + aup;
+
+        if (a % 2 == 0) {
+            color.style.backgroundColor = 'rgb(209, 139, 71)';
+        } else {
+            color.style.backgroundColor = 'rgb(255, 206, 158)';
+        }
+    }));
+}
+
+colorBoard();
