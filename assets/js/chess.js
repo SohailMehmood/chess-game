cbdaee586f9830c3661c49e0352b31341cbe10af
SohailMehmood <sohail_mehmood@hotmail.com>
2024-02-08 16:50:41 +0000

    Adjust styling of pawns

--- assets/js/chess.js
+++ assets/js/chess.js
@@ -3,8 +3,8 @@
 function insertImages() {
     document.querySelectorAll('.square').forEach(image => {
         if (image.innerText.length !== 0) {
-            if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
-                image.innerHTML = `${image.innerText} <img class='all-img all-pawn' src="assets/images/${image.innerText}.png" alt="${image.innerText}">`;
+            if (image.innerText == 'w-pawn' || image.innerText == 'b-pawn') {
+                image.innerHTML = `${image.innerText} <img class='all-img' src="assets/images/${image.innerText}.png" alt="${image.innerText}">`;
                 image.style.cursor = 'pointer';
             } else {
                 image.innerHTML = `${image.innerText} <img class='all-img' src="assets/images/${image.innerText}.png" alt="${image.innerText}">`;
