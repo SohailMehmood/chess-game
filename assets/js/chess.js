5242c0a66ad91e32314a5500e978090b658391bb
SohailMehmood <sohail_mehmood@hotmail.com>
2024-02-15 08:34:25 +0000

    Add event listeners to squares

--- assets/js/chess.js
+++ assets/js/chess.js
@@ -37,106 +37,13 @@ function colorBoard() {
 
 colorBoard();
 
-tog = 1
-
-document.querySelectorAll('.box').forEach(item => {
-
-
-            item.addEventListener('click', function () {
-
-                    if (item.style.backgroundColor == 'greenyellow' && item.innerText.length == 0) {
-                        tog = tog + 1
-                    } else if (item.style.backgroundColor == 'greenyellow' && item.innerText.length !== 0) {
-
-                        document.querySelectorAll('.box').forEach(i => {
-                            if (i.style.backgroundColor == 'blue') {
-                                blueId = i.id
-                                blueText = i.innerText
-
-                                document.getElementById(blueId).innerText = ''
-                                item.innerText = blueText
-                                coloring()
-                                insertImage()
-                                tog = tog + 1
-
-                            }
-                        })
-                    }
-
-
-
-                    getId = item.id
-                    arr = Array.from(getId)
-                    arr.shift()
-                    aside = eval(arr.pop())
-                    arr.push('0')
-                    aup = eval(arr.join(''))
-                    a = aside + aup
-
-                    //function to display the available paths for all pieces
-
-                    function whosTurn(toggle) {
-                        // PAWN
-
-                        if (item.innerText == `${toggle}pawn`) {
-                            item.style.backgroundColor = 'blue';
-
-                            if (tog % 2 !== 0 && aup < 800) {
-                                // First move for white pawns
-                                if (document.getElementById(`b${a + 100}`).innerText.length == 0) {
-                                    document.getElementById(`b${a + 100}`).style.backgroundColor = 'greenyellow';
-                                    if (document.getElementById(`b${a + 200}`).innerText.length == 0 && aup < 300) {
-                                        document.getElementById(`b${a + 200}`).style.backgroundColor = 'greenyellow';
-                                    }
-                                }
-                                if (aside < 8 && document.getElementById(`b${a + 100 + 1}`).innerText.length !== 0) {
-                                    document.getElementById(`b${a + 100 + 1}`).style.backgroundColor = 'greenyellow';
-                                }
-                                if (aside > 1 && document.getElementById(`b${a + 100 - 1}`).innerText.length !== 0) {
-                                    document.getElementById(`b${a + 100 - 1}`).style.backgroundColor = 'greenyellow';
-                                }
-                            }
-
-                            if (tog % 2 == 0 && aup > 100) {
-                                // First move for black pawns
-                                if (document.getElementById(`b${a - 100}`).innerText.length == 0) {
-                                    document.getElementById(`b${a - 100}`).style.backgroundColor = 'greenyellow';
-                                    if (document.getElementById(`b${a - 200}`).innerText.length == 0 && aup > 600) {
-                                        document.getElementById(`b${a - 200}`).style.backgroundColor = 'greenyellow';
-                                    }
-                                }
-                                if (aside < 8 && document.getElementById(`b${a - 100 + 1}`).innerText.length !== 0) {
-                                    document.getElementById(`b${a - 100 + 1}`).style.backgroundColor = 'greenyellow';
-                                }
-                                if (aside > 1 && document.getElementById(`b${a - 100 - 1}`).innerText.length !== 0) {
-                                    document.getElementById(`b${a - 100 - 1}`).style.backgroundColor = 'greenyellow';
-                                }
-                            }
-                            // Second move for pawns
-                            if (tog % 2 !== 0 && aup >= 800) {
-                                if (document.getElementById(`b${a + 100}`).innerText.length == 0) {
-                                    document.getElementById(`b${a + 100}`).style.backgroundColor = 'greenyellow';
-                                }
-                                if (aside < 8 && document.getElementById(`b${a + 100 + 1}`).innerText.length !== 0) {
-                                    document.getElementById(`b${a + 100 + 1}`).style.backgroundColor = 'greenyellow';
-                                }
-                                if (aside > 1 && document.getElementById(`b${a + 100 - 1}`).innerText.length !== 0) {
-                                    document.getElementById(`b${a + 100 - 1}`).style.backgroundColor = 'greenyellow';
-                                }
-                            }
-                            if (tog % 2 == 0 && aup <= 100) {
-                                if (document.getElementById(`b${a - 100}`).innerText.length == 0) {
-                                    document.getElementById(`b${a - 100}`).style.backgroundColor = 'greenyellow';
-                                }
-                                if (aside < 8 && document.getElementById(`b${a - 100 + 1}`).innerText.length !== 0) {
-                                    document.getElementById(`b${a - 100 + 1}`).style.backgroundColor = 'greenyellow';
-                                }
-                                if (aside > 1 && document.getElementById(`b${a - 100 - 1}`).innerText.length !== 0) {
-                                    document.getElementById(`b${a - 100 - 1}`).style.backgroundColor = 'greenyellow';
-                                }
-                            }
-                        }
-                    }
-                }
+// Adding event listeners to the squares
+document.querySelectorAll('.square').forEach(square => {
+    square.addEventListener('click', () => {
+        if (square.innerText.length !== 0) {
+            if (square.innerText == 'w-pawn' || square.innerText == 'b-pawn') {
+                square.style.backgroundColor = 'rgb(255, 0, 0)';
             }
-        )
+        }
+    });
+});
