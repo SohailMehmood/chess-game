96f52b73f040a6ac453c47d80d70f5c169c503b9
SohailMehmood <sohail_mehmood@hotmail.com>
2024-02-15 10:58:54 +0000

    Add highlight for available moves when a pawn is selected

--- assets/js/chess.js
+++ assets/js/chess.js
@@ -1,4 +1,7 @@
 /* jshint esversion: 11, jquery: true */
+
+let selectedPiece = null;
+
 // Inserting images
 function insertImages() {
     document.querySelectorAll('.square').forEach(image => {
@@ -6,9 +9,12 @@ function insertImages() {
             if (image.innerText == 'w-pawn' || image.innerText == 'b-pawn') {
                 image.innerHTML = `${image.innerText} <img class='all-img all-pawn' src="assets/images/${image.innerText}.png" alt="${image.innerText}">`;
                 image.style.cursor = 'pointer';
+                image.dataset.hasMoved = 'false';
+                image.dataset.piece = image.innerText; // Add this line
             } else {
                 image.innerHTML = `${image.innerText} <img class='all-img' src="assets/images/${image.innerText}.png" alt="${image.innerText}">`;
                 image.style.cursor = 'pointer';
+                image.dataset.piece = image.innerText; // Add this line
             }
         }
     });
@@ -16,7 +22,7 @@ function insertImages() {
 
 insertImages();
 
-//Coloring the board
+// Coloring the board
 function colorBoard() {
     const squares = document.querySelectorAll('.square');
 
@@ -34,16 +40,83 @@ function colorBoard() {
     });
 }
 
-
 colorBoard();
 
 // Adding event listeners to the squares
 document.querySelectorAll('.square').forEach(square => {
     square.addEventListener('click', () => {
-        if (square.innerText.length !== 0) {
-            if (square.innerText == 'w-pawn' || square.innerText == 'b-pawn') {
-                square.style.backgroundColor = 'rgb(255, 0, 0)';
+        if (square.dataset.piece) {
+            if (square.dataset.piece == 'w-pawn' || square.dataset.piece == 'b-pawn') {
+                selectedPiece = square.dataset.piece;
+                const isFirstMove = square.dataset.hasMoved === 'false';
+                highlightMoves(square, isFirstMove);
+                square.dataset.hasMoved = 'true';
             }
         }
     });
 });
+
+function highlightMoves(square, isFirstMove) {
+    const index = Array.from(square.parentNode.children).indexOf(square);
+    const row = Math.floor(index / 8);
+    const col = index % 8;
+
+    if (selectedPiece == 'w-pawn') {
+        if (isFirstMove && isEmptySquare(row - 2, col)) {
+            highlightSquare(row - 2, col);
+        }
+        if (isEmptySquare(row - 1, col)) {
+            highlightSquare(row - 1, col);
+        }
+        if (isEnemyPiece(row - 1, col - 1)) {
+            highlightCapture(row - 1, col - 1);
+        }
+        if (isEnemyPiece(row - 1, col + 1)) {
+            highlightCapture(row - 1, col + 1);
+        }
+    } else if (selectedPiece == 'b-pawn') {
+        if (isFirstMove && isEmptySquare(row + 2, col)) {
+            highlightSquare(row + 2, col);
+        }
+        if (isEmptySquare(row + 1, col)) {
+            highlightSquare(row + 1, col);
+        }
+        if (isEnemyPiece(row + 1, col - 1)) {
+            highlightCapture(row + 1, col - 1);
+        }
+        if (isEnemyPiece(row + 1, col + 1)) {
+            highlightCapture(row + 1, col + 1);
+        }
+    }
+}
+
+
+function highlightSquare(row, col) {
+    if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+        const square = document.querySelector(`.row:nth-child(${row + 1}) .square:nth-child(${col + 1})`);
+        square.style.backgroundColor = 'rgb(0, 255, 0)';
+    }
+}
+
+function highlightCapture(row, col) {
+    if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+        const square = document.querySelector(`.row:nth-child(${row + 1}) .square:nth-child(${col + 1})`);
+        square.style.backgroundColor = 'rgb(255, 0, 0)';
+    }
+}
+
+function isEmptySquare(row, col) {
+    if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+        const square = document.querySelector(`.row:nth-child(${row + 1}) .square:nth-child(${col + 1})`);
+        return !square.dataset.piece;
+    }
+    return false;
+}
+
+function isEnemyPiece(row, col) {
+    if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+        const square = document.querySelector(`.row:nth-child(${row + 1}) .square:nth-child(${col + 1})`);
+        return square.dataset.piece && square.dataset.piece != selectedPiece;
+    }
+    return false;
+}
